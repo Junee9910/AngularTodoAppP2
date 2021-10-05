c25271084f0864d620bd7bc6747663c4c7f0bc52
JuneeDinh <dinhttnhung9910@gmail.com>
2021-10-05 11:53:30 +0700

    edit completed

--- src/app/users/todo-edit/todo-edit.component.ts
+++ src/app/users/todo-edit/todo-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-todo-edit',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoEditComponent implements OnInit {
 
-  constructor() { }
+  userId:any;
+  userDetails:any;
+  editUserForm:FormGroup=new FormGroup({});
+  dataLoaded:boolean=false;
+
+  constructor(private activateRoute:ActivatedRoute, private userService:UserService, private formBuilder:FormBuilder, private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
+    this.dataLoaded=false;
+    this.activateRoute.params.subscribe(data=>{
+      this.userId=data.id;
+    });
+
+    if(this.userId !== ''){
+      this.userService.viewUser(this.userId)
+      .toPromise()
+      .then(data=>{
+        this.userDetails=data;
+        Object.assign(this.userDetails,data);
+
+        this.editUserForm=this.formBuilder.group({
+          'name':new FormControl(this.userDetails.name),
+          'email':new FormControl(this.userDetails.email)
+        })
+        this.dataLoaded=true;
+      })
+      .catch(err=>{
+        console.log(err);
+      })
+    }
   }
 
+  updateUser(){
+   this.userService.updateUser(this.userId, this.editUserForm.value).subscribe(data=>{
+    this._snackBar.open("User updated successfully");
+  }, err=>{
+    this._snackBar.open("Unable to updated user");
+  })
+  }
 }
